257878596d6602f8c0040cc594aa657f7c4fddfe
agi-yads <22f2001715@ds.study.iitm.ac.in>
2024-08-12 22:02:54 +0530

    New ChatBot

--- st_app.py
+++ st_app.py
@@ -9,12 +9,16 @@ import tempfile
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
 from google.generativeai import GenerationConfig, GenerativeModel
-from langchain_community.vectorstores import FAISS 
+from langchain_community.vectorstores import FAISS  # For Vector Embeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+# from langchain_core.runnables import RunnablePassthrough
 from langchain_core.messages import AIMessage, HumanMessage
 from dotenv import load_dotenv
+
+from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled, NoTranscriptFound, VideoUnavailable
 
 
 def get_pdf_text(pdf_docs):
@@ -36,6 +40,29 @@ def get_pdf_text(pdf_docs):
         return None
 
 
+def extract_transcript_details(video_id):
+    print(f"Video ID: {video_id}")
+    try:
+        transcript_text = YouTubeTranscriptApi.get_transcript(video_id, languages=["en", "hn"])
+        transcript = ""
+        for i in transcript_text:
+            transcript += " " + i["text"]
+
+        return transcript
+    except TranscriptsDisabled:
+        st.error("Transcripts are disabled for this video.")
+        return None
+    except NoTranscriptFound:
+        st.error("No transcript found for this video.")
+        return None
+    except VideoUnavailable:
+        st.error("Video is unavailable.")
+        return None
+    except Exception as e:
+        st.error(f"Internal Error Occurred\n Details- {str(e)}")
+        return None
+
+
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
     chunks = text_splitter.split_text(text)
@@ -50,9 +77,9 @@ def get_vector_store(text_chunks):
 
 def get_conversation_chat_chain():
     prompt_template = """
-    You are a highly advanced AI agent designed to assist users by providing detailed and comprehensive answers based on the context of a PDF document. 
-    Your goal is to deliver in-depth responses that thoroughly cover the user's question, referencing relevant sections, data, or insights from the document.
-    If the user’s question relates to the topic discussed in the document but the specific answer is not available in the provided context, explicitly state that the provided context does not contain the answer. 
+    You are a highly advanced AI agent designed to assist users by providing detailed and comprehensive answers based on the context of a PDF documents or Video. 
+    Your goal is to deliver in-depth responses that thoroughly cover the user's question, referencing relevant sections, data, or insights from the documents or Video content.
+    If the user’s question relates to the topic discussed in the document or video but the specific answer is not available in the provided context, explicitly state that the provided context does not contain the answer. 
     Then, offer the correct and detailed answer based on your knowledge or database in 500-750 words without providing incorrect information.
     
     Additional Instructions:
@@ -153,12 +180,11 @@ def generate_suggested_questions(description, num_ques):
         )
 
         prompt = f"""
-        Given the following summary of content of the document,
+        Given the following summary of content of the document or video,
         **Summary:**
         {description}
 
-        Generate {num_ques} thoughtful and engaging questions that might be asked and their answer can be found in the document content.
-        All the {num_ques} questions should be in the similar order of the content
+        Generate {num_ques} questions that might be asked and their answer can be found in the document or video content.
         The questions should focus on key topics, interesting points.
 
         Follow the JSON schema.<JSONSchema>{json.dumps(required_response_schema)}</JSONSchema>
@@ -177,7 +203,7 @@ def generate_suggested_questions(description, num_ques):
 
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = [
-        AIMessage(content="Hi There, I'm Chat with Documents AI Assistant. Ask me anything about your Documents."),
+        AIMessage(content="Hi There, I'm Chat with Documents or Video AI Assistant. Ask me anything about your Documents/Video."),
     ]
 
 if 'sq_response' not in st.session_state:
@@ -193,19 +219,37 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
 def main():
-    st.set_page_config("Chat With Multiple PDFs", page_icon="favicon.ico")
+    st.set_page_config("Chat With Multiple PDFs or Video", page_icon="favicon.ico")
 
-    st.header("Chat with PDFs")
+    st.title("Chat with PDFs or Video")
 
     with st.sidebar:
         st.title("Set Up:")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on Analyze The PDFs", accept_multiple_files=True)
+
+        video_toggle = st.toggle("Chat With Video")
+        pdf_docs = None
+        youtube_video_id = None
+
+        if video_toggle:
+            youtube_link = st.text_input("Enter YouTube Video Link:",
+                                         placeholder="https://www.youtube.com/watch?v=video_url_id")
+            youtube_video_id = ""
+            if youtube_link.startswith("https://www.youtube.com/watch?v="):
+                youtube_video_id = youtube_link.split("=")[1]
+            elif youtube_link.startswith("https://youtu.be/"):
+                youtube_video_id = youtube_link.split("/")[-1]
+            # print(youtube_video_id)
+
+            if youtube_link:
+                st.video(f"https://youtu.be/{youtube_video_id}")
+        else:
+            pdf_docs = st.file_uploader("Upload your PDF Files and Click on Analyze The PDFs", accept_multiple_files=True, type=['pdf'])
         want_suggested_questions = st.toggle(label="Suggested Questions")
         num_of_ques = 0
         if want_suggested_questions:
             num_of_ques = st.slider(label="Number of Suggested Questions", min_value=0, max_value=15, value=0)
         # print(len(pdf_docs.read()))
-        if st.button("Analyze The PDF"):
+        if st.button("Analyze"):
             if pdf_docs:
                 st.session_state.analyzed_video = False
                 with st.spinner("Analyzing with AI..."):
@@ -226,17 +270,39 @@ def main():
                             st.error("Unable To Analyze the Document!")
                     except Exception as e:
                         st.error(f"Error during Document analysis: {e}")
+            elif youtube_video_id:
+                st.session_state.analyzed_video = False
+                with st.spinner("Analyzing with AI..."):
+                    try:
+                        transcription = extract_transcript_details(youtube_video_id)
+                        if transcription:
+                            text_chunks = get_text_chunks(transcription)
+                            get_vector_store(text_chunks)
+
+                            st.success("Analyzed the Video")
+                            st.session_state.analyzed_video = True
+                            st.session_state.num_sq = 0
+                            st.session_state.chat_history = [
+                                AIMessage(
+                                    content="Hi There, I'm Chat with Video AI Assistant. Ask me anything about your Documents."),
+                            ]
+                            # st.balloons()
+                        else:
+                            st.error("Unable To Analyze the Video!")
+                    except Exception as e:
+                        st.error(f"Error during Video analysis: {e}")
             else:
-                st.warning(body="Please, Upload the Document First")
-        if st.session_state.num_sq != num_of_ques:
+                st.warning(body="Please, Upload the Document / Video Url First")
+        if st.session_state.num_sq != num_of_ques and st.session_state.analyzed_video:
             st.session_state.num_sq = num_of_ques
+            desc_res = None
             if want_suggested_questions and num_of_ques > 0:
-                summary_prompt = "Describe the content and summarize the document in important points"
+                summary_prompt = "Describe the content and summarize the document/video in important points in 1000 words."
                 desc_res = get_user_input(
                     user_question=summary_prompt,
                     chat_history=[
                         AIMessage(
-                            content="Hi There, I'm Chat with Documents AI Assistant. Ask me anything about your Documents.")
+                            content="Hi There, I'm Chat with Documents AI Assistant. Ask me anything about your Documents/Video.")
                     ]
                 )
 
@@ -251,7 +317,7 @@ def main():
             with st.chat_message("Human"):
                 st.markdown(message.content)
 
-    user_ques = st.chat_input(placeholder="Ask me anything about your Document.")
+    user_ques = st.chat_input(placeholder="Ask me anything about your Document/Video.")
 
     if st.session_state.analyzed_video and want_suggested_questions and num_of_ques > 0 and st.session_state.sq_response:
         try:
@@ -274,7 +340,8 @@ def main():
 
         if selected_question:
             selected_ques = selected_question
-            user_ques = selected_ques
+            if user_ques is None:
+                user_ques = selected_ques
 
     if user_ques is not None and user_ques.strip() != "":
         print(user_ques)
@@ -297,7 +364,6 @@ def main():
             except Exception as e:
                 error_msg = str(e)
                 st.error(error_msg)
-
 
 
 if __name__ == "__main__":
